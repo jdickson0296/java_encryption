da0b4738784b130c223f4436e6550155f580048c
jdickson0296 <jdickson0296@gmail.com>
2019-09-30 13:48:04 -0700

    added try exception for incorrect password

--- src/com/company/AESFileDecryption.java
+++ src/com/company/AESFileDecryption.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.security.spec.KeySpec;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
@@ -13,48 +14,54 @@ import javax.crypto.spec.SecretKeySpec;
 class AESFileDecryption {
      static void Decrypt(String des_file, String file_format, String password) throws Exception {
 
-        // reading the salt
-        // user should have secure mechanism to transfer the
-        // salt, iv and password to the recipient
-        FileInputStream saltFis = new FileInputStream("salt.enc");
-        byte[] salt = new byte[8];
-        saltFis.read(salt);
-        saltFis.close();
+        try {
+           // reading the salt
+           // user should have secure mechanism to transfer the
+           // salt, iv and password to the recipient
+           FileInputStream saltFis = new FileInputStream("salt.enc");
+           byte[] salt = new byte[8];
+           saltFis.read(salt);
+           saltFis.close();
 
-        // reading the iv
-        FileInputStream ivFis = new FileInputStream("iv.enc");
-        byte[] iv = new byte[16];
-        ivFis.read(iv);
-        ivFis.close();
+           // reading the iv
+           FileInputStream ivFis = new FileInputStream("iv.enc");
+           byte[] iv = new byte[16];
+           ivFis.read(iv);
+           ivFis.close();
 
-        SecretKeyFactory factory = SecretKeyFactory
-                .getInstance("PBKDF2WithHmacSHA1");
-        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
-                256);
-        SecretKey tmp = factory.generateSecret(keySpec);
-        SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+           SecretKeyFactory factory = SecretKeyFactory
+                   .getInstance("PBKDF2WithHmacSHA1");
+           KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
+                   256);
+           SecretKey tmp = factory.generateSecret(keySpec);
+           SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
 
-        // file decryption
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
-        FileInputStream fis = new FileInputStream(des_file);
+           // file decryption
+           Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+           cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+           FileInputStream fis = new FileInputStream(des_file);
 
-        FileOutputStream fos = new FileOutputStream("decrypted_file."+file_format);
-        byte[] in = new byte[64];
-        int read;
-        while ((read = fis.read(in)) != -1) {
-            byte[] output = cipher.update(in, 0, read);
-            if (output != null)
-                fos.write(output);
+           FileOutputStream fos = new FileOutputStream("decrypted_file." + file_format);
+           byte[] in = new byte[64];
+           int read;
+           while ((read = fis.read(in)) != -1) {
+              byte[] output = cipher.update(in, 0, read);
+              if (output != null)
+                 fos.write(output);
+           }
+
+           byte[] output = cipher.doFinal();
+           if (output != null)
+              fos.write(output);
+           fis.close();
+           fos.flush();
+           fos.close();
+
+           System.out.println("File Decrypted");
         }
 
-        byte[] output = cipher.doFinal();
-        if (output != null)
-            fos.write(output);
-        fis.close();
-        fos.flush();
-        fos.close();
-
-        System.out.println("File Decrypted");
+        catch (BadPaddingException e) {
+           System.out.println("Incorrect key used for decryption");
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         String password = "password123";
-        AESFileEncryption.Encrypt("plainfile.txt", password);
+//        AESFileEncryption.Encrypt("plainfile.txt", password);
         AESFileDecryption.Decrypt("encryptedfile.des", "txt", password);
     }
 }
